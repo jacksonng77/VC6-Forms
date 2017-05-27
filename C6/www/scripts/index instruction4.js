adc2a23a972534168f91e12568ffdb8b2b99c605
Jackson Ng <jacksonng@outlook.com>
2017-05-27 15:51:58 +0800

    complete

---
+++ C6/www/scripts/index instruction4.js
@@ -0,0 +1,29 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    $(document).ready(function () {
+        $("#btnCalculate").bind("click", function () {
+
+            var hoursWorked; 
+            var hourlyRate;
+
+            hoursWorked = $("#txtHoursWorked").val();
+            hourlyRate = $("#txtHourlyRate").val();
+
+            var timeDayWorked = getRadioValue("rdoTimedayworked");
+
+            var mywage = timeDayWorked * wages(hoursWorked, hourlyRate);
+
+            localStorage.setItem("hoursworked", hoursWorked);
+            localStorage.setItem("hourlyrate", hourlyRate);
+            localStorage.setItem("actualwage", mywage);
+
+            window.location = "result.html";
+        });
+    });
+
+} )();
