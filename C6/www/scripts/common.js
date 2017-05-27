6ddeec1d5096140c313071fdb65c9d4c615cd665
Jackson Ng <jacksonng@outlook.com>
2017-05-27 13:56:55 +0800

    Initial Release

---
+++ C6/www/scripts/common.js
@@ -0,0 +1,16 @@
+﻿function wages(hoursWorked, hourlyRate) {
+    var totalsalary = hoursWorked * hourlyRate;
+    return totalsalary;
+}
+
+function home() {
+    window.location = "index.html";
+}
+
+function hello() {
+    window.location = "hello.html";
+}
+
+function goodbye() {
+    window.location = "goodbye.html";
+}
